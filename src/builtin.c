1e8104a70b58aa146bc72923bb6f4f30f496890f
flyndann <dannyflynn@gmx.com>
2025-09-16 12:02:32 +0200

    similar case

--- src/builtin.c
+++ src/builtin.c
@@ -60,61 +60,6 @@ void	ft_list_to_env_array(t_dat *data)
 	data->evs[i] = NULL;
 }
 
-char	*ft_get_cmd_path_nested(const char *cmd)
-{
-	if (access(cmd, X_OK) == 0)
-		return (ft_strdup(cmd));
-	return (NULL);
-}
-
-char	*ft_get_cmd_path(t_dat *d, const char *cmd, int i)
-{
-	char	*full;
-
-	if (!cmd || *cmd == '\0')
-		return (NULL);
-	if (cmd[0] == '/' || (cmd[0] == '.' && cmd[1] == '/'))
-		return (ft_get_cmd_path_nested(cmd));
-	d->tmp1 = ft_get_val_from_list(d->ev, "PATH");
-	if (!d->tmp1)
-		return (NULL);
-	d->avs = ft_split(d->tmp1, ':');
-	while (d->avs && d->avs[i])
-	{
-		full = ft_join_path(d->avs[i], (char *)cmd);
-		if (access(full, X_OK) == 0)
-		{
-			ft_free_string_array(d->avs);
-			return (full);
-		}
-		free(full);
-		i++;
-	}
-	ft_free_string_array(d->avs);
-	return (NULL);
-}
-
-void	ft_cmd_not_found(char *cmd)
-{
-	char	*prefix;
-	char	*suffix;
-
-	prefix = "minishell: ";
-	suffix = ": command not found\n";
-	write(2, prefix, ft_strlen(prefix));
-	write(2, cmd, ft_strlen(cmd));
-	write(2, suffix, ft_strlen(suffix));
-	exit(127);
-}
-
-void	ft_cmd_error(t_dat *data, char *line)
-{
-	(void)line;
-	ft_free_string_array(data->evs);
-	data->evs = NULL;
-	return ;
-}
-
 void	ft_external_functions(t_dat *data, char *line)
 {
 	char	***cmd;
