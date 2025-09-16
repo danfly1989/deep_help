3f34bb08493ca5f17f5bb28b223439b2b24395e3
flyndann <dannyflynn@gmx.com>
2025-09-16 11:41:31 +0200

    barely reaching 109 on occasion after exit changes

--- src/update.c
+++ src/update.c
@@ -78,34 +78,6 @@ void	ft_echo(char **arr, size_t k)
 		printf("\n");
 }
 
-void	ft_exit_numeric_error(char *arg)
-{
-	write(2, "minishell: exit: ", 18);
-	write(2, arg, ft_strlen(arg));
-	write(2, ": numeric argument required\n", 29);
-}
-
-void	ft_exit(t_dat *data, size_t k)
-{
-	int	status;
-
-	if (data->xln[k + 1] && data->xln[k + 2])
-	{
-		write(2, "minishell: exit: too many arguments\n", 36);
-		return ;
-	}
-	rl_clear_history();
-	if (data->xln[k + 1] == NULL)
-		ft_cleanup_exit(data, 0);
-	if (ft_is_number(data->xln[k + 1]) == 0)
-	{
-		ft_exit_numeric_error(data->xln[k + 1]);
-		ft_cleanup_exit(data, 255);
-	}
-	status = ft_atoi(data->xln[k + 1]);
-	ft_cleanup_exit(data, status % 256);
-}
-
 void	ft_env(t_dat *data)
 {
 	t_va	*cur;
